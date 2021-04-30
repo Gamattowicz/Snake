f9f8bb51d1895017e36d9f7b7a6144dd47e8c5e5
Gamattowi <p.romanczuk31@gmail.com>
2021-04-30 07:55:00 +0200

    add displaying max score

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 import random
 
 # SIZE OF SCREEN =
-WIDTH, HEIGHT = 1000, 750
+WIDTH, HEIGHT = 1100, 750
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('SNAKE')
 pygame.init()
@@ -25,11 +25,14 @@ class Board():
         title_text = title_font.render(text, True, (255, 255, 255))
         surface.blit(title_text, (self.screen_width // 2 - title_text.get_width()/2, self.start_y // 2 - title_text.get_height()/2))
 
-    def draw_score(self, surface):
+    def draw_sides_text(self, surface):
         score_font = pygame.font.SysFont('arial', 40)
         score_text = score_font.render('Score: 0', True, (255, 255, 255))
-        surface.blit(score_text,
-                     (self.start_x // 2 - score_text.get_width() / 2, self.screen_height // 4 - score_text.get_height() / 2))
+        surface.blit(score_text, (self.start_x // 2 - score_text.get_width() / 2,
+                                  self.screen_height // 4 - score_text.get_height() / 2))
+        max_score_text = score_font.render('Max score: 0', True, (255, 255, 255))
+        surface.blit(max_score_text, (self.screen_width - self.start_x // 2 - max_score_text.get_width() / 2,
+                                      self.screen_height // 4 - max_score_text.get_height() / 2))
 
     def create_squares(self, surface):
         for i in range(len(self.squares)):
@@ -127,7 +130,7 @@ class Apple():
 def main():
     board = Board(20, 20, 30, WIDTH, HEIGHT)
     board.draw_title('SNAKE', WIN)
-    board.draw_score(WIN)
+    board.draw_sides_text(WIN)
     apple = Apple((255, 0, 0))
     apple.place_apple(board, apple.generate_location(board))
     snake = Snake((0, 255, 0))
