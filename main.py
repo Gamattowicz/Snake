8b3ec40a8d414e67251cebbfef43f2483f7ec9d8
Gamattowi <p.romanczuk31@gmail.com>
2021-04-30 08:08:36 +0200

    fix bug with place apple inside snake body

--- main.py
+++ main.py
@@ -125,8 +125,10 @@ class Apple():
         squares = [[(j, i) for j in range(board.columns)] for i in range(board.rows)]
         squares = [j for sub in squares for j in sub]
 
-        location = random.choice(squares)
-        return location
+        for i in range(10):
+            location = random.choice(squares)
+            if board.squares[location[0]][location[1]] != (0, 255, 0):
+                return location
 
     def place_apple(self, board, location):
         board.squares[location[0]][location[1]] = self.color
