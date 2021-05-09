2632969c360e62ba7bc67bbee53b2930d1f1b709
Gamattowi <p.romanczuk31@gmail.com>
2021-05-09 13:02:02 +0200

    move player class to separate module

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import pygame
 import sys
-import csv
-from datetime import date
 from menu import draw_menu, pause, ACTIVE_COLOR, BACKGROUND_COLOR, TEXT_COLOR, TITLE_FONT, SIDE_FONT
 from leaderboard import get_leaderboard
 from apple import Apple
+from player import Player
 
 # SIZE OF SCREEN =
 WIDTH, HEIGHT = 1100, 750
@@ -195,52 +194,6 @@ class Snake:
     def collision_check(self, surface, board, player):
         if board.squares[self.loc_y][self.loc_x] == self.color:
             board.draw_name(surface, player, board)
-
-
-class Player:
-    def __init__(self):
-        self.score = 0
-        self.timer = 0
-        self.name = ''
-        self.speed = 1
-        self.mode = 1
-        self.start_speed = 1
-
-    def format_timer(self):
-        mins = self.timer // 60
-        formatted_mins = f'0{mins}' if mins < 10 else mins
-        secs = self.timer - mins * 60
-        formatted_secs = f'0{secs}' if secs < 10 else secs
-        formatted_timer = f'{formatted_mins}:{formatted_secs}'
-
-        return formatted_timer
-
-    def save_score(self, format_timer):
-        with open('scores.csv', 'a+') as f:
-            f.seek(0)
-            data = f.read(100)
-            if len(data) > 0:
-                f.write('\n')
-            f.write(
-                f'{self.name},{str(self.score)},{round(self.speed, 1)},{format_timer()},{date.today()}')
-
-    @staticmethod
-    def get_max_score():
-        rows = []
-        with open('scores.csv', 'a+') as f:
-            f.seek(0)
-            reader = csv.reader(f, delimiter=',')
-            for row in reader:
-                rows.append(int(row[1]))
-        if len(rows) > 0:
-            max_score = sorted(rows, reverse=True)
-            return max_score[0]
-
-    def restart_stats(self):
-        self.score = 0
-        self.timer = 0
-        self.name = ''
-        self.speed = self.start_speed
 
 
 def main(player):
---
+++ player.py
@@ -0,0 +1,48 @@
+import csv
+from datetime import date
+
+
+class Player:
+    def __init__(self):
+        self.score = 0
+        self.timer = 0
+        self.name = ''
+        self.speed = 1
+        self.mode = 1
+        self.start_speed = 1
+
+    def format_timer(self):
+        mins = self.timer // 60
+        formatted_mins = f'0{mins}' if mins < 10 else mins
+        secs = self.timer - mins * 60
+        formatted_secs = f'0{secs}' if secs < 10 else secs
+        formatted_timer = f'{formatted_mins}:{formatted_secs}'
+
+        return formatted_timer
+
+    def save_score(self, format_timer):
+        with open('scores.csv', 'a+') as f:
+            f.seek(0)
+            data = f.read(100)
+            if len(data) > 0:
+                f.write('\n')
+            f.write(
+                f'{self.name},{str(self.score)},{round(self.speed, 1)},{format_timer()},{date.today()}')
+
+    @staticmethod
+    def get_max_score():
+        rows = []
+        with open('scores.csv', 'a+') as f:
+            f.seek(0)
+            reader = csv.reader(f, delimiter=',')
+            for row in reader:
+                rows.append(int(row[1]))
+        if len(rows) > 0:
+            max_score = sorted(rows, reverse=True)
+            return max_score[0]
+
+    def restart_stats(self):
+        self.score = 0
+        self.timer = 0
+        self.name = ''
+        self.speed = self.start_speed
